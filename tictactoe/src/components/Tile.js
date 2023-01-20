9fc3a8342d8461e039cc77c432c10ec087ca226b
BLacey99 <Blake.M.Lacey@gmail.com>
2023-01-19 20:57:34 -0600

    Still broken

--- tictactoe/src/components/Tile.js
+++ tictactoe/src/components/Tile.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useContext,  useState } from 'react';
 
 
 export default function Tile(props){
     const [clicked, setClick] = useState(false);
-    
+    const [classToggle, setClass] = useState(false);
+    const [content, setContent] = useState('');
 
+    const turnO = 'o';
+    const turnX = 'x';
 
-   
+    const value = useContext(props.turn);
     // useEffect( () =>{
     //     console.log(clicked);
     // })
@@ -15,8 +18,27 @@ export default function Tile(props){
     const clicker = () =>{
         setClick(true);
         console.log("Fixed value: " + !(clicked))
+        //setClass(true);
+        if(classToggle === true){
+            setClass(false);
+        }else{setClass(true)}
+        console.log("Fixed: " + !(classToggle))
+        setContent(props.turn)
+
+        if (props.turn === 'x'){
+            props.setTurn('o');
+            console.log(props.turn);
+        }else{
+            props.setTurn('x');
+            console.log(props.turn);
+        }
+        
     }
     return(
-        <div className='tile' onClick={clicker}>{props.entry}</div>
+        <div 
+        className={classToggle ? 'clicked' : 'tile'} 
+        onMouseEnter={clicker}>
+        <p>{value}</p>
+        </div>
     )
 }
--- tictactoe/src/components/Tiles.js
+++ tictactoe/src/components/Tiles.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Tile from './Tile';
 import  "./Tiles.css";
+import { useEffect, useState } from 'react';
+
 
 export default function Tiles(props){
+
+    const[turn, setTurn] = useState('x');
+
     const tileArr = [];
-  
+
     (async function generateArr(){
         for(let i = 1; i < 10; i++){
             tileArr.push(i);
@@ -12,13 +17,21 @@ export default function Tiles(props){
     }
     })();
 
+
+
     
       
-        const mapper = tileArr.map((entry) => <Tile entry={entry}>{entry}</Tile>)
+        const mapper = tileArr.map((entry) => <Tile setTurn = {setTurn} turn = {turn} entry={entry}>{entry}</Tile>)
 
     return(
-        <div className='playingBoard'>
+        <div>
+               <h2>{turn}</h2>
+            <br></br>
+      
+        <div className='playingBoard' turn = {turn}>
+         
            {mapper}
+        </div>
         </div>
     )
 }
